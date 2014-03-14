19966e5ecfb4810afdbd8783ad0fe3addf33a998
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-03-14 23:05:48 +0400

    Show cam options on click

--- scripts/pre_controller.js
+++ scripts/pre_controller.js
@@ -3,3 +3,21 @@ var isMobile = isMobile();
 if (!isMobile) {
   //redirect('/');
 }
+
+var scan  = $('#scan'),
+  camList = $('#camList');
+
+MediaStreamTrack.getSources(function (sources) {
+  var c = 1;
+  sources.forEach(function (source, i) {
+    if (source.kind === 'video') {
+      camList.append('<option value="' + sources.id +'"> Camera ' + (c++) + '</option>');
+    }
+  });
+});
+
+
+$("#scan").click(function () {
+  $(this).hide();
+  camList.show();
+});
