1b812a4514c835881a0c47a646b731d1ceb9238d
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2014-03-14 23:10:01 +0400

    game qr and url on create

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,26 @@ if (isMobile()) {
   redirect('/');
 }
 
+$('#game_url').hide();
+
 var peer = new Peer({ key: 'z0bavx5ok1emi', debug: 2 });
 
 peer.on('open', function(id) {
+
+  var url = window.location.href;
+
   window.location.hash = id;
+
+  new QRCode('game_qr', {
+    text: url,
+    width: 232,
+    height: 232,
+    colorDark: '#000000',
+    colorLight: '#F2F2F2',
+    correctLevel: QRCode.CorrectLevel.H
+  });
+
+
+  $('#game_url').attr('href', url).show();
+
 });
